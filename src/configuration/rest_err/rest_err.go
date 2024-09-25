f6847a9f396d66e875a6167449c3806e3443af42
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2024-09-25 19:46:11 -0300

    salvando projeto

---
+++ src/configuration/rest_err/rest_err.go
@@ -0,0 +1,68 @@
+package rest_err
+
+import "net/http"
+
+type ResErr struct {
+	Message string   `json:"message"`
+	Err     string   `json:"error"`
+	Code    int64    `json:"code"`
+	Causes  []Causes `json:"causes"`
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func (r *ResErr) Error() string {
+	return r.Message
+}
+
+func NewRestErr(message, err string, code int64, causes []Causes) *ResErr {
+	return &ResErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+func NewBadRequestError(message string) *ResErr {
+	return &ResErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *ResErr {
+	return &ResErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewInternalServerError(message string) *ResErr {
+	return &ResErr{
+		Message: message,
+		Err:     "Internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *ResErr {
+	return &ResErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *ResErr {
+	return &ResErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
---
+++ src/controller/test.go
@@ -0,0 +1,7 @@
+package controller
+
+import "github.com/Ckaiique/crud-go-mvc/src/configuration/rest_err"
+
+func test(message string) (err *rest_err.ResErr) {
+	return err.Code
+}
